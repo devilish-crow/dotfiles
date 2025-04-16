595ed0c0fd327832cde98858454ad8de8b338caf
crow <crow@alpine.my.domain>
2025-04-16 22:41:43 +0300

    Added AI hype

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,28 @@
 return {
   {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false,
+    dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- provided by NvChad
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  {
     "stevearc/conform.nvim",
     event = 'BufWritePre',
     opts = require "configs.conform",
