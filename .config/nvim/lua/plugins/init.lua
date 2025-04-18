ad384038a6f09deb0e29da4396a954b567ecfed6
crow <crow@alpine.my.domain>
2025-04-18 15:20:02 +0300

    Added AI hype x2

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,6 +3,18 @@ return {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false,
+    opts = {
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
+          max_tokens = 8192,
+        },
+      },
+    },
     dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
